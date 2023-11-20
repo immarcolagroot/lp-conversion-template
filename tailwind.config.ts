7f055b0ebc6d76b6bfdccf9a8a6d35b498f24175
João Marcolino <otalmarcolino@gmail.com>
2023-11-20 14:45:45 -0300

    feat: set colors of tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,22 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      white: "#FFF",
+      black: "#000",
+      primary: "#A335D7",
+      gray: "#D9D9D9",
+      "black-50": "#00000080",
+      "black-75": "#000000bf",
+      "gray-50": "#D9D9D980",
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
