f052cafbe94e230eeb097e993ee65f0c7ed58b27
João Marcolino <otalmarcolino@gmail.com>
2023-11-21 18:03:46 -0300

    refactor: adding new components of header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,29 @@
+import Image from "next/image";
+
+import CompanyLogo from "@/assets/images/LOGO.svg";
+import NavButton from "@/components/NavButton";
+
 export default function Header() {
   return (
     <header>
-      <img />
+      <div className="w-full h-16 flex justify-between items-center px-8 py-5">
+        <div className="flex-1">
+          <Image src={CompanyLogo} alt="Logo" width={90} height={45} />
+        </div>
+        <div className="w-72 flex gap-4">
+          {/* TODO implement onClick method */}
+          <NavButton title="PROCESSOS" />
+          <NavButton title="DEPOIMENTOS" />
+          <NavButton title="SOBRE" />
+        </div>
+        <div className="flex-1 flex justify-end">
+          <button className="h-10 px-4 py-2 bg-primary bg-opacity-40 rounded-2xl border border-black hover:bg-opacity-25">
+            <span className="text-black text-base font-bold">
+              MARCAR CALL GRATUITA
+            </span>
+          </button>
+        </div>
+      </div>
     </header>
   );
 }
