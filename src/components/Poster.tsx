fd37622cc93cfd529972cba291c389839f05ecca
João Marcolino <otalmarcolino@gmail.com>
2023-11-21 18:02:58 -0300

    feat: create Poster component

---
+++ src/components/Poster.tsx
@@ -0,0 +1,9 @@
+export default function Poster() {
+  return (
+    <div className="w-full h-8 bg-primary flex justify-center items-center">
+      <span className="text-white text-xs font-bold">
+        ACESSOS LIMITADOS, MARQUE SUA CALL AGORA
+      </span>
+    </div>
+  );
+}
