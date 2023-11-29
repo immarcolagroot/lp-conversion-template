e151106560db759b09d33f78ba55eda8f3cff27c
ElBigas <elbigas@protonmail.com>
2023-11-29 14:49:50 -0300

    feat(beneficiossection): Criar componente BeneficiosSection
    - Criado o componente BeneficiosSection
    - Implementada a estrutura básica e o layout do componente BeneficiosSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import HeroSection from '@/components/HeroSection'
 import ClientesSection from "@/components/ClientesSection";
 import EstudoDeCasoSection from "@/components/EstudoDeCasoSection";
-import ImagemMontanha from '@/assets/images/beneficio.svg'
 import CardBeneficio from '@/components/CardBeneficio';
+import Button from '@/components/Button';
+import BeneficiosSection from '@/components/BeneficiosSection';
 
 export default function Home() {
   return (
@@ -14,48 +15,7 @@ export default function Home() {
 
       <EstudoDeCasoSection />
 
-      <div className="pt-16 pb-8 px-8 flex flex-col justify-center text-center">
-
-        <h1 className="text-black text-6xl font-extrabold leading-[62px] pt-10">Um título chamativo sobre benefícios</h1>
-
-        <div className="text-center px-36 pt-8 text-black text-opacity-75 text-lg">
-          <span className="font-normal">
-            Corem ipsum dolor sit amet, consectetur adipiscing elit. Nunc{" "}
-          </span>
-          <span className="font-bold">vulputate libero et velit interdum</span>
-          <span className="font-normal">
-            , ac aliquet odio mattis. Class aptent taciti{" "}
-          </span>
-          <span className="font-bold">sociosqu ad litora torquent</span>
-          <span className="font-normal">
-            {" "}
-            per conubia nostra, per inceptos himenaeos.
-          </span>
-        </div>
-
-        <div className="pt-12 px-8 flex justify-center gap-10">
-
-          <CardBeneficio
-            imagem={ImagemMontanha}
-            titulo="Beneficio #1"
-            comentario="Corem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit "
-          />
-
-          <CardBeneficio
-            imagem={ImagemMontanha}
-            titulo="Beneficio #1"
-            comentario="Corem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit "
-          />
-
-          <CardBeneficio
-            imagem={ImagemMontanha}
-            titulo="Beneficio #1"
-            comentario="Corem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit "
-          />
-
-        </div>
-
-      </div>
+      <BeneficiosSection />
 
     </>
   );
---
+++ src/components/BeneficiosSection.tsx
@@ -0,0 +1,56 @@
+import Button from "./Button";
+import CardBeneficio from "./CardBeneficio";
+import Selo from '@/assets/images/beneficio.svg'
+
+export default function EstudoDeCasoSection() {
+    return (
+        <div className="pt-16 pb-8 px-8 flex flex-col justify-center text-center">
+
+        <h1 className="text-black text-6xl font-extrabold leading-[62px] pt-10">Um título chamativo sobre benefícios</h1>
+
+        <div className="text-center px-36 pt-8 text-black text-opacity-75 text-lg">
+          <span className="font-normal">
+            Corem ipsum dolor sit amet, consectetur adipiscing elit. Nunc{" "}
+          </span>
+          <span className="font-bold">vulputate libero et velit interdum</span>
+          <span className="font-normal">
+            , ac aliquet odio mattis. Class aptent taciti{" "}
+          </span>
+          <span className="font-bold">sociosqu ad litora torquent</span>
+          <span className="font-normal">
+            {" "}
+            per conubia nostra, per inceptos himenaeos.
+          </span>
+        </div>
+
+        <div className="pt-12 px-8 flex justify-center gap-10">
+
+          <CardBeneficio
+            imagem={Selo}
+            titulo="Beneficio #1"
+            comentario="Corem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit "
+          />
+
+          <CardBeneficio
+            imagem={Selo}
+            titulo="Beneficio #1"
+            comentario="Corem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit "
+          />
+
+          <CardBeneficio
+            imagem={Selo}
+            titulo="Beneficio #1"
+            comentario="Corem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit "
+          />
+
+        </div>
+
+        <div className='pt-16'>
+          <Button
+            title="Marcar call gratuita"
+          />
+        </div>
+
+      </div>
+    );
+  }
